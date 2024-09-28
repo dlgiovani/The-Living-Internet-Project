58bc8a4fe63b623620e5f0b1f7b68cf76012471b
Giovani <gdrosdalima@gmail.com>
2024-09-28 02:39:48 -0300

    Update App.svelte

--- the-living-internet/src/App.svelte
+++ the-living-internet/src/App.svelte
@@ -1,30 +1,60 @@
 <script lang="ts">
-  let search = '';
-  let database = [
-	"https://dlgiovani.github.io",
-  ]
-  let results = [];
 
-  function handleSearch() {
-    // compare search with database of indexed websites
-    // present the results
-    Promise.all(database.filter((item) => item.includes(search)).map(async(result) => {
-      let request = await fetch(`${result}/living_internet/index.json`);
-      let data = await request.json();
-      results = [...results, { url: `${result}${data.root}`, thumb: `${result}/living_internet${data.thumbnail_url}`, description: data.description }]
-    }));
+  let database = [
+	{
+          url: "https://dlgiovani.github.io",
+	  path_to_li: "/",
+	  status: null,
+	  description: null,
+	  thumbnail_url: null
+	},
+  ]
+
+  let favourites = localStorage.getItem("favourites") | null;
+  if (favourites) {
+    database = [...favourites, ...database];
+  } // append favourites to the beggining of the db and it really does not matter that they'll be fetched twice
+  
+  document.addEventListener('DOMContentLoaded', async () => {
+    for (let i = 0; i < database.length; i++) {
+     // TODO: make validation to also cause fetch if the data is too old or if it is a favourite.
+      if ((!database[i].status) || database[i].status >= 400) {
+        const result = await fetch(database[i].url + database[i].path_to_li + '/living_internet/index.json');
+	const data = await result.json();
+	database[i].status = result.status_code;
+	database[i].description = data.description;
+	database[i].thumbnail_url = database[i].url + 
+				    database[i].path_to_li + 
+				    "/living_internet" + data.thumbnail_url;
+      }
+    }
+
+  });
+
+  let search = '';
+  let results = [];
+  async function handleSearch() {
+    console.log(search);
+    results = [];
+    const batchSize = 10;
+    for (let i = 0; i < database.length; i += batchSize) {
+      const batch = database.slice(i, i + batchSize).map((o) => ({...o}));
+      const batchResults = batch.filter((item) => 
+      			item.url.includes(search) || item.description.includes(search));
+      results = [...results, ...batchResults];
+    }
   }
 
 </script>
 
 <main>
   <input type="text" bind:value={search}/>
-  <button type="submit" on:click={handleSearch}>search!</button>
+  <button type="submit" on:click={async () => handleSearch()}>search!</button>
 
   {#each results as result}
     <a target="_blank" href={result.url} class="result">
       <div>
-        <img src={result.thumb} alt={result}/>
+        <img src={result.thumbnail_url} alt={result}/>
 	<div>
 	  <h2>{result.url}</h2>
           <p>{result.description}</p>
