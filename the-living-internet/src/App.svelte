1b15ed1a65f08bc2441286338cb6be286067814d
Giovani <gdrosdalima@gmail.com>
2024-10-06 22:23:16 -0300

    Adjusted resource structure

--- the-living-internet/src/App.svelte
+++ the-living-internet/src/App.svelte
@@ -2,11 +2,10 @@
 
 let database = [
   {
-    url: "https://dlgiovani.github.io",
-    path_to_li: "/", // this will be replaced by a Living Internet OpenGraph tag, <meta property="liog:feed" content="{feed_url}"/>
-    status: null,
-    description: null,
-    thumbnail_url: null
+    resource_url: "https://dlgiovani.github.io/living_internet/index.json",
+    last_successful_fetch: null,
+    last_fetch: null,
+    content: null
   },
 ]
 
@@ -15,36 +14,53 @@ if (favourites) {
   database = [...favourites, ...database];
 } // append favourites to the beggining of the db and it really does not matter that they'll be fetched twice
 
-document.addEventListener('DOMContentLoaded', async () => {
+const fetchData = async () => {
   for (let i = 0; i < database.length; i++) {
-    // TODO: make validation to also cause fetch if the data is too old or if it is a favourite.
-    if ((!database[i].status) || database[i].status >= 400) {
-      const result = await fetch(database[i].url + database[i].path_to_li + '/living_internet/index.json');
-      const data = await result.json();
-      database[i].status = result.status_code;
-      database[i].description = data.description.slice(0, 256); // we don't wanna get too big amir8 ladies
-      database[i].thumbnail_url = database[i].url + 
-        database[i].path_to_li + 
-        "/living_internet" + data.thumbnail_url;
+    console.log(database[i].last_successful_fetch);
+    if (Date.parse(database[i].last_successful_fetch < (Date.now() - 1000 * 60 * 10)) || !database[i].last_successful_fetch ) {
+      const result = await fetch(database[i].resource_url);
+      database[i].last_fetch = Date.now()
+      if (result.status <= 200) {
+        database[i].last_successful_fetch = Date.now()
+       
+        database[i].content = await result.json();
+      }
     }
   }
+  console.log(database);
+}
 
-});
+fetchData();
 
 let search = '';
-let results = [];
+let siteResults = [];
+let postResults = [];
 async function handleSearch() {
-  console.log(search);
-  results = [];
+  siteResults = [];
+  postResults = [];
   let upperSearch = search.toUpperCase();
   const batchSize = 10;
   for (let i = 0; i < database.length; i += batchSize) {
     const batch = database.slice(i, i + batchSize).map((o) => ({...o}));
-    const batchResults = batch.filter((item) => 
-      item.url.toUpperCase().includes(upperSearch) || item.description.toUpperCase().includes(upperSearch));
-    results = [...results, ...batchResults];
+
+    const batchResults = batch.filter((item) => {
+      if (item.content.url.toUpperCase().includes(upperSearch) || item.content.title.toUpperCase().includes(upperSearch)) {
+        const { feed, ...content } = item.content;
+        siteResults = [...siteResults, content]
+      }
+    }
+    );
+
+    const batchContentResults = batch.map((item) => 
+      item.content.feed.filter((post) =>  {    
+        if (JSON.stringify(post).toUpperCase().includes(upperSearch)) {
+
+          postResults = [...postResults, { ...post, origin: item.content.title + ' | ' + item.content.url}]
+        }
+      }));
   }
-  console.log(results); 
+
+  console.log(JSON.stringify(siteResults));
 }
 
 </script>
@@ -53,16 +69,28 @@ async function handleSearch() {
   <input type="text" bind:value={search}/>
   <button type="submit" on:click={async () => handleSearch()}>search!</button>
 
-  {#each results as result}
+  {#each siteResults as result}
     <a target="_blank" href={result.url} class="result">
+      <h2>{result?.title}</h2>
+      <p>{result.url}</p>
+    </a>
+  {/each}
+
+  {#each postResults as pResult}
+    <a target="_blank" href={pResult?.url} class="result">
       <div>
-        <img src={result.thumbnail_url} alt={result}/>
+        {#if (pResult?.images) }
+          <img width=200 src={pResult?.images[0]?.url} alt={pResult?.images[0]?.alt}/>
+        {/if}
         <div>
-          <h2>{result.url}</h2>
-          <p>{result.description}</p>
+          <p>From: {pResult.origin}</p>
+          <h2>{pResult.title}</h2>
+          <p>{pResult.url}</p>
+          <p>{pResult.description}</p>
         </div>
       </div>
     </a>
+
   {/each}
 </main>
 
@@ -76,6 +104,12 @@ input {
 
 .result {
   display: block;
+  border: solid 1px white;
+  margin: 10px 0;
+  padding: 6px;
+  &:hover {
+    background-color: #ffffff11;
+  }
   & div {
     display: flex;
     flex-direction: row;
